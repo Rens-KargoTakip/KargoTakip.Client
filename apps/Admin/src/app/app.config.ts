291d9686a416db943d6454068032dc182641c077
omeruren <omer.uren19@gmail.com>
2026-01-28 15:44:33 +0300

    chanded Date provide

--- apps/Admin/src/app/app.config.ts
+++ apps/Admin/src/app/app.config.ts
@@ -1,16 +1,18 @@
 import {
   ApplicationConfig,
+  LOCALE_ID,
   provideExperimentalZonelessChangeDetection,
   provideZoneChangeDetection,
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { DatePipe } from '@angular/common';
+import { DatePipe, registerLocaleData } from '@angular/common';
 import { authInterceptor } from './interceptors/auth.interceptor';
 import { errInterceptor } from './interceptors/err.interceptor';
 import { provideNgxMask } from 'ngx-mask';
-
+import localeTr from '@angular/common/locales/tr';
+registerLocaleData(localeTr);
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(appRoutes),
@@ -18,5 +20,6 @@ export const appConfig: ApplicationConfig = {
     provideExperimentalZonelessChangeDetection(), // standalone api
     DatePipe,
     provideNgxMask(),
+    { provide: LOCALE_ID, useValue: 'tr-TR' },
   ],
 };
